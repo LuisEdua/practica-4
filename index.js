85b346c41666b5f0d3fd2270ee3d15f6462f6e05
Luis Eduardo <lefarrera@gmail.com>
2024-06-07 02:14:31 -0600

    [UPDATE] Message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World! x7');
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
